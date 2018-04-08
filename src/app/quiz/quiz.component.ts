a928ea5762b8e0b189fd55a5c7a27395fe11c8e3
sameeranihathe <sameeranihathe@gmail.com>
2018-04-08 23:00:16 +0530

    result componennt created

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -12,6 +12,16 @@ export class QuizComponent implements OnInit {
   constructor(private router: Router, private quizService: QuizService) { }
 
   ngOnInit() {
+    if (parseInt(localStorage.getItem('seconds')) > 0) {
+      this.quizService.seconds = parseInt(localStorage.getItem('seconds'));
+      this.quizService.qnProgress = parseInt(localStorage.getItem('qnProgress'));
+      this.quizService.qns = JSON.parse(localStorage.getItem('qns'));
+      if (this.quizService.qnProgress == 10)
+        this.router.navigate(['/result']);
+      else
+        this.startTimer();
+    }
+    else{
     this.quizService.seconds = 0;
     this.quizService.qnProgress = 0;
 
@@ -23,16 +33,25 @@ export class QuizComponent implements OnInit {
       }
 
       );
+    }
   }
 
   startTimer(){
     this.quizService.timer = setInterval(()=>{
       this.quizService.seconds++;
+      localStorage.setItem('seconds', this.quizService.seconds.toString());
     }, 1000);
   }
 
   Answer(qID, choice){
-    
+    this.quizService.qns[this.quizService.qnProgress].Answer = choice;
+    localStorage.setItem('qns', JSON.stringify(this.quizService.qns));
+    this.quizService.qnProgress++;
+    localStorage.setItem('qnProgress', this.quizService.qnProgress.toString());
+    if(this.quizService.qnProgress ==0){
+      clearInterval(this.quizService.timer);
+      this.router.navigate(['/result']);
+    }
   }
 
   
--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { QuizService } from '../shared/quiz.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-result',
@@ -8,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 export class ResultComponent implements OnInit {
 
  
-  constructor() { }
+  constructor(private quizService: QuizService, private router: Router ) { }
 
   ngOnInit() {
+    this.quizService.getAnswers().subscribe(
+      (data: any)=>{
+        this.quizService.correctAnswerCount =0;
+        this.quizService.qns.forEach((e,i)=>{
+          if(e.answer==data[i])
+            this.quizService.correctAnswerCount++;
+            e.correct  = data[i];
+
+        });
+      }
+
+    );
+  }
+  OnSubmit(){
+    this.quizService.submitScore().subscribe(()=>{
+
+    });
+  }
+  restart(){
+    this.router.navigate(['/quiz']);
   }
 
 }
