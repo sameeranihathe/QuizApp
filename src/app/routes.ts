cdf7c1e0b5d2f4deb4754471e5c4db07746ac84c
sameeranihathe <sameeranihathe@gmail.com>
2018-04-08 09:13:07 +0530

    user authentication added

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,10 +2,11 @@ import {Routes} from '@angular/router'
 import { RegisterComponent } from './register/register.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { ResultComponent } from './result/result.component';
+import { AuthGuard } from './auth/auth.guard';
 
 export const appRoutes :Routes = [
     {path:'register', component:RegisterComponent},
-    {path:'quiz', component:QuizComponent},
-    {path:'result', component:ResultComponent},
+    {path:'quiz', component:QuizComponent, canActivate: [AuthGuard]},
+    {path:'result', component:ResultComponent, canActivate: [AuthGuard]},
     {path:'', redirectTo:'/register', pathMatch:'full'}
 ];
