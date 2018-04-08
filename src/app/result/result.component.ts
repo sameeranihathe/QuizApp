67e66c732997c6bf16b99953cf0f5e8d0e99671e
sameeranihathe <sameeranihathe@gmail.com>
2018-04-08 23:05:49 +0530

    result component error fixed

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -9,29 +9,42 @@ import { Router } from '@angular/router';
 })
 export class ResultComponent implements OnInit {
 
- 
-  constructor(private quizService: QuizService, private router: Router ) { }
+
+
+  constructor(private quizService: QuizService, private router: Router) { }
 
   ngOnInit() {
-    this.quizService.getAnswers().subscribe(
-      (data: any)=>{
-        this.quizService.correctAnswerCount =0;
-        this.quizService.qns.forEach((e,i)=>{
-          if(e.answer==data[i])
-            this.quizService.correctAnswerCount++;
-            e.correct  = data[i];
+    if (parseInt(localStorage.getItem('qnProgress')) == 10) {
+      this.quizService.seconds = parseInt(localStorage.getItem('seconds'));
+      this.quizService.qnProgress = parseInt(localStorage.getItem('qnProgress'));
+      this.quizService.qns = JSON.parse(localStorage.getItem('qns'));
 
-        });
-      }
-
-    );
+      this.quizService.getAnswers().subscribe(
+        (data: any) => {
+          this.quizService.correctAnswerCount = 0;
+          this.quizService.qns.forEach((e, i) => {
+            if (e.answer == data[i])
+              this.quizService.correctAnswerCount++;
+            e.correct = data[i];
+          });
+        }
+      );
+    }
+    else
+      this.router.navigate(['/quiz']);
   }
-  OnSubmit(){
-    this.quizService.submitScore().subscribe(()=>{
 
+
+  OnSubmit() {
+    this.quizService.submitScore().subscribe(() => {
+      this.restart();
     });
   }
-  restart(){
+
+  restart() {
+    localStorage.setItem('qnProgress', "0");
+    localStorage.setItem('qns', "");
+    localStorage.setItem('seconds', "0");
     this.router.navigate(['/quiz']);
   }
 
